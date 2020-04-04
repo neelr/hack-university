c3254047c604dcf562ed420daadf69583d9039fb
Neel Redkar <neel.redkar@outlook.com>
2020-04-03 17:44:19 -0700

    add title

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,13 @@ import theme from "../components/theme";
 import Nav from "../components/nav";
 import Footer from "../components/footer";
 import { Flex } from "rebass";
+import Head from "next/head"
 
 export default ({ Component, props }) => (
   <ThemeProvider theme={theme}>
+    <Head>
+      <title>Hack University!</title>
+    </Head>
     <Flex flexDirection="column" minHeight="100vh">
       <Nav />
       <Flex mb="auto">
