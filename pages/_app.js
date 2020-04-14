dcc0bb76dbf508ea6387e09cf8f126c28a016d01
Neel Redkar <neel.redkar@outlook.com>
2020-04-13 21:42:12 -0700

    add google verification

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,7 @@ export default ({ Component, props }) => (
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
+      <meta name="google-site-verification" content="lbE7qTArPeKcKU55QWflkokeDJmcHXeEZGO0yXRxhj4" />
     </Head>
     <Flex flexDirection="column" minHeight="100vh">
       <Nav />
